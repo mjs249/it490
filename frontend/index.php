1b2f6a295539bdbbecfed43e8d2498bd1d9eb106
mike <mjs249@njit.edu>
2024-02-21 13:52:38 -0500

    more style changes, bug fixes.

--- frontend/index.php
+++ frontend/index.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//error_reporting(E_ALL);
 
 require_once('/home/mike/it490/path.inc');
 require_once('/home/mike/it490/get_host_info.inc');
@@ -41,25 +41,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['username']) && isset($
 </head>
 <body>
     <h2>Login Form</h2>
+    <form action="index.php" method="post">
     <?php if (isset($_SESSION['flash_message'])): ?>
         <p style="color: green;"><?php echo $_SESSION['flash_message']; ?></p>
         <?php unset($_SESSION['flash_message']); ?>
     <?php endif; ?>
     <?php if (!empty($errorMessage)) { echo "<p style='color:red'>$errorMessage</p>"; } ?>
-    <form action="index.php" method="post" onsubmit="return validateLoginForm()">
+
          Username: <input type="text" name="username" id="username" required><br>
 	Password:
     <div class="password-container">
         <input type="password" name="password" id="password" required>
         <span id="togglePassword" class="toggle-password"><i class="fas fa-eye"></i></span>
-        <span id="capsLockWarning" class="tooltip">Caps Lock is ON!</span>
+        <span id="capsLockWarning" class="tooltip" style="display: none;">Caps Lock is ON!</span>
     </div>
     <br>
     <input type="submit" value="Login">
-    </form>
     <p>Don't have an account? <a href="register.php">Register here</a>.</p>
+    </form>
 
     <script src="login.js"></script>
 </body>
 </html>
-
