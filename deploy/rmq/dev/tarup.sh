57b26a5daeaf5bed874dacf7c84c44897445685e
Mike <mjs249@njit.edu>
2024-04-12 14:43:56 -0400

    fixed a typo

--- deploy/rmq/dev/tarup.sh
+++ deploy/rmq/dev/tarup.sh
@@ -4,7 +4,7 @@ SOURCE_DIR1="/home/mike/it490/rabbitmq"
 SOURCE_DIR2="/home/mike/it490/DB"
 DEPLOYMENT_SERVER="mike@192.168.192.188"
 TARGET_DIR="/home/mike/Desktop/"
-TARBALL_NAME="rmq_db_files_$(date +%Y-%m-%d_%H-%M-%S).tar.gz"
+TARBALL_NAME="rmq_files_$(date +%Y-%m-%d_%H-%M-%S).tar.gz"
 FULL_TARBALL_PATH="${TARGET_DIR}${TARBALL_NAME}"
 
 echo "Creating a single tarball of both RMQ and DB directories..."
