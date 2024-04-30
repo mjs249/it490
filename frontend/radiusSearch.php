68572f7d7516aeb02f09196e372c68f92689283d
nuhauddin <145134926+nuhauddin@users.noreply.github.com>
2024-04-29 23:11:34 -0400

    Update radiusSearch.php

--- frontend/radiusSearch.php
+++ frontend/radiusSearch.php
@@ -68,7 +68,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['term'], $_POST['locati
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Profile</title>
+    <title>Search by Location</title>
     <link rel="stylesheet" href="home.css">
 </head>
 <body>
