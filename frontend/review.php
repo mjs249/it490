982d85d6462539465bca6a7a5f3aca39b8bf25d8
nuhauddin <145134926+nuhauddin@users.noreply.github.com>
2024-04-29 23:18:10 -0400

    Update review.php

--- frontend/review.php
+++ frontend/review.php
@@ -97,7 +97,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Profile</title>
+    <title>Leave a Review</title>
     <link rel="stylesheet" href="home.css">
 </head>
 <body>
